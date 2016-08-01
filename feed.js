82db82b333e7f079999bcd2ae9df08a79b6d5463
Kevin Bui <bui.kevin@live.com>
2016-08-01 14:36:31 -0700

    Opens the link when item is clicked

--- feed.js
+++ feed.js
@@ -1,9 +1,13 @@
 function previewInMainPanel() {
     let div = document.getElementById('feed-active');
+    let preview = document.getElementById('preview');
     if (div !== null) {
         div.id = 'feed';
     }
     let url = this.getAttribute('link');
     this.id = 'feed-active';
+
+    let currentLink = this.getAttribute('link');
+    preview.setAttribute('src', currentLink);
     console.log(this);
 }
