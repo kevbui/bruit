f612ec5a73196b61dffb74a4ffcdf0d93daf6c6f
Kevin Bui <bui.kevin@live.com>
2016-08-07 23:05:40 -0700

    Created events for handling the button events

---
+++ js/titlebar.js
@@ -0,0 +1,50 @@
+(function () {
+
+  const remote = require('electron').remote;
+
+  function init() {
+    document.getElementById("min-btn").addEventListener("click", function (e) {
+      const window = remote.getCurrentWindow();
+      window.minimize();
+    });
+
+    document.getElementById("max-btn").addEventListener("click", function (e) {
+      const window = remote.getCurrentWindow();
+      if (!window.isMaximized()) {
+        window.maximize();
+      } else {
+        window.unmaximize();
+      }
+    });
+
+    document.getElementById("close-btn").addEventListener("click", function (e) {
+      const window = remote.getCurrentWindow();
+      window.close();
+    });
+  };
+
+  document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+      init();
+    }
+  };
+
+})();
+
+function submitAddDialog() {
+  let data = document.getElementById('feed-input').value;
+  feednami.load(data, (result) => {
+    if (result.error) {
+      document.getElementById('feed-input').value = 'Bad URL';
+    }
+    else {
+      let parent = document.getElementById('sidebar');
+      let div = document.createElement('div');
+      div.className = 'sidebar-feed';
+      div.innerText = result.feed.meta.title;
+      div.onclick = loadFeed;
+      div.setAttribute("link", result.feed.meta.xmlurl);
+      parent.appendChild(div);
+    }
+  })
+}
